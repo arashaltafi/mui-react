386023078f792701b0593e7841602f363314fa00
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-01 23:35:57 +0330

    A

--- src/sample2/component/Add.jsx
+++ src/sample2/component/Add.jsx
@@ -1,6 +1,6 @@
 import { useStyles } from "../style";
 import { Tooltip, Fab, Modal } from '@mui/material';
-import { Add as AddIcon } from '@mui/icons-material';
+import { Add as AddIcon, Cancel } from '@mui/icons-material';
 import { useState } from "react";
 import { Container } from "@material-ui/core";
 
@@ -25,9 +25,12 @@ function Add() {
                 aria-describedby="modal-modal-description"
             >
                 <Container className="w-screen sm:w-1/3 h-screen sm:h-1/3 bg-white absolute inset-0 m-auto">
-                    <p>
-                        این یک مدل می باشد.
-                    </p>
+                    <div className="flex flex-col items-center justify-center mt-6">
+                        <p className="">
+                            این یک مدل می باشد.
+                        </p>
+                        <Cancel className="mt-4 text-red-500" onClick={ handleClose } />
+                    </div>
                 </Container>
             </Modal>
         </>
