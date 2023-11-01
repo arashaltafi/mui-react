8dab30de6e4c5c528cfba142bddc6336b16cdb2a
آرش الطافي <altafi.a@Nahad.ir>
2023-11-01 12:04:36 +0330

    A

--- src/sample2/component/Navbar.jsx
+++ src/sample2/component/Navbar.jsx
@@ -1,16 +1,17 @@
-import { Search } from "@mui/icons-material";
+import { Search, Mail } from "@mui/icons-material";
 import { useStyles } from "../style";
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import { InputBase } from "@material-ui/core";
+import { Badge } from "@mui/material";
 
 function Navbar() {
     const classes = useStyles();
 
     return (
         <AppBar position="fixed" color="primary">
-          <Toolbar>
+          <Toolbar className="flex flex-row justify-between items-center">
             {/* <Typography variant="h6" component="h2" className="hidden sm:block">
                 وبلاگ آرش
             </Typography>
@@ -25,7 +26,12 @@ function Navbar() {
             </Typography>
             <div className={ classes.search }>
               <Search />
-              <InputBase placeholder="جستجو ..." />
+              <InputBase placeholder="جستجو ..." className={ classes.input } />
+            </div>
+            <div classes = { classes.icon }>
+              <Badge badgeContent={4} color="error">
+                <Mail />
+              </Badge>
             </div>
           </Toolbar>
         </AppBar>
--- src/sample2/style.js
+++ src/sample2/style.js
@@ -1,3 +1,4 @@
+import { alpha } from '@material-ui/core';
 import { makeStyles } from '@mui/styles';
 
 export const useStyles = makeStyles((theme) => ({
@@ -18,5 +19,28 @@ export const useStyles = makeStyles((theme) => ({
         [theme.breakpoints.down("sm")]: {
             display: "block"
         }
+    },
+    toolbar: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    search: {
+        display: "flex",
+        alignItems: "center",
+        backgroundColor: alpha("#ffffff", 0.15),
+        '&:hover': {
+            backgroundColor: alpha("#ffffff", 0.25)
+        },
+        borderRadius: 4,
+        width: "50%",
+    },
+    input: {
+        color: "white",
+        marginRight: 20
+    },
+    icon: {
+
     }
 }));
