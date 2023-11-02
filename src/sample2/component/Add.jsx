d8a35719a883181ae9606bd7025e657656c7136c
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-02 15:23:21 +0330

    A

--- src/sample2/component/Add.jsx
+++ src/sample2/component/Add.jsx
@@ -1,5 +1,5 @@
 import { useStyles } from "../style";
-import { Tooltip, Fab, Modal, Box, TextField, MenuItem, RadioGroup, Radio, FormLabel, FormControlLabel } from '@mui/material';
+import { Tooltip, Fab, Modal, Box, TextField, MenuItem, Select, RadioGroup, Radio, InputLabel, FormLabel, FormControl, FormControlLabel, Button } from '@mui/material';
 import { Add as AddIcon, Cancel } from '@mui/icons-material';
 import { useState } from "react";
 import { Container } from "@material-ui/core";
@@ -24,39 +24,42 @@ function Add() {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Container className="w-screen sm:w-1/3 h-screen sm:h-1/2 bg-white absolute inset-0 m-auto">
+                <Container className="w-screen sm:w-1/3 h-screen sm:h-2/3 bg-white absolute inset-0 m-auto">
                     <div className="flex flex-col items-center justify-center mt-6">
                         <Box
                             component="form"
                             noValidate
                             autoComplete="off"
-                            className="w-full"
+                            className="w-full p-3"
                         >
-                            <div>
-                                <TextField error id="standard-basic" label="عنوان" variant="standard" className="w-full" />
+                            <div className="mb-6">
+                                <TextField id="standard-basic" placeholder="عنوان" variant="standard" className="w-full" />
                             </div>
-                            <div>
+                            <div className="mb-6">
                                 <TextField id="outline-multiline-static" label="پیام شما" defaultValue="داستان خودت را بگو ..." variant="outlined" multiline rows={5} className="w-full" />
                             </div>
-                            <div>
-                                <TextField
-                                    id="outlined-select-currency"
-                                    select
-                                    label="نوع دسترسی"
-                                    helperText="نوع دسترسی"
-                                >
-                                    <MenuItem key="public" value="عمومی">
-                                        عمومی
-                                    </MenuItem>
-                                    <MenuItem key="private" value="خصوصی">
-                                        خصوصی
-                                    </MenuItem>
-                                    <MenuItem key="friends" value="نمایش برای دوستان">
-                                        نمایش برای دوستان
-                                    </MenuItem>
-                                </TextField>
+                            <div className="mb-6">
+                                <FormControl fullWidth variant="standard">
+                                    <InputLabel id="demo-simple-select-label" className="w-screen">انتخاب</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        value="123"
+                                        label="نوع دسترسی"
+                                    >
+                                        <MenuItem key="public" value="عمومی">
+                                            عمومی
+                                        </MenuItem>
+                                        <MenuItem key="private" value="خصوصی">
+                                            خصوصی
+                                        </MenuItem>
+                                        <MenuItem key="friends" value="نمایش برای دوستان">
+                                            نمایش برای دوستان
+                                        </MenuItem>
+                                    </Select>
+                                </FormControl>
                             </div>
-                            <div>
+                            <div className="mb-6">
                                 <FormLabel id="demo-radio-buttons-group-label">دسترسی کامنت گذاری</FormLabel>
                                 <RadioGroup>
                                     <FormControlLabel value="everybody" control={<Radio size="small" />} label="برای همه" />
@@ -65,11 +68,14 @@ function Add() {
                                     <FormControlLabel disabled value="premium" control={<Radio size="small" />} label="سفارشی" />
                                 </RadioGroup>
                             </div>
+                            <div className="flex flex-row gap-x-2 mb-6">
+                                <Button variant="outlined" color="primary">ارسال</Button>
+                                <Button variant="outlined" color="secondary" onClick={handleClose}>انصراف</Button>
+                            </div>
                         </Box>
-                        <Cancel className="mt-4 text-red-500" onClick={handleClose} />
                     </div>
-                </Container>
-            </Modal>
+                </Container >
+            </Modal >
         </>
     );
 }
